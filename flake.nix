b7b8be127b1000335fa955aee0e6262e9132c106
Alkaid <a1kari8@outlook.com>
2026-02-16 14:00:28 +0800

    add zen

--- flake.nix
+++ flake.nix
@@ -26,6 +26,12 @@
       url = "github:AvengeMedia/dms-plugin-registry";
       inputs.nixpkgs.follows = "nixpkgs";
     };
+
+    zen-browser = {
+      url = "github:0xc000022070/zen-browser-flake";
+      inputs.nixpkgs.follows = "nixpkgs";
+    };
+
     # nixos-grub-themes = {
     #   url = "github:jeslie0/nixos-grub-themes";
     #   inputs.nixpkgs.follows = "nixpkgs";
--- os/program/browser.nix
+++ os/program/browser.nix
@@ -1,6 +1,7 @@
-{ pkgs, ... }:
+{ pkgs, inputs, ... }:
 {
   environment.systemPackages = with pkgs; [
     google-chrome
+    inputs.zen-browser.packages."${pkgs.system}".default
   ];
 }
