535b3692015285ff4f663295d03e099193184388
Alkaid <a1kari8@outlook.com>
2026-02-20 12:14:29 +0800

    add mirror

--- flake.nix
+++ flake.nix
@@ -40,8 +40,10 @@
 
   nixConfig = {
     substituters = [
-      "https://cache.nixos.org"
+      "https://mirrors.sjtug.sjtu.edu.cn/nix-channels/store"
+      "https://mirrors.nju.edu.cn/nix-channels/store"
       "https://mirrors.tuna.tsinghua.edu.cn/nix-channels/store"
+      "https://cache.nixos.org"
     ];
     trusted-public-keys = [ "cache.nixos.org-1:6NCHdD59X431o0gWypbMrAURkbJ16ZPMQFGspcDShjY=" ];
   };
