549d7134e26d8e5c0cbaf9ba57eb1ae2c9c9362f
Alkaid <a1kari8@outlook.com>
2026-02-20 12:14:21 +0800

    niri patch

--- os/program/niri.nix
+++ os/program/niri.nix
@@ -4,23 +4,41 @@
   config,
   ...
 }:
+let
+  niriCommit = "a2a52911757cb3b497db9407592f9b4c439571ea";
+in
 {
-  # services.xserver.enable = true;
   services.xserver.xkb.layout = "us";
 
   programs.niri = {
     enable = true;
     useNautilus = true;
-    package = pkgs.niri.overrideAttrs (oldAttrs: {
-      doCheck = false;
+    package = pkgs.niri.overrideAttrs (oldAttrs: rec {
+      # pname = "niri";
+      # version = "unstable-${builtins.substring 0 7 niriCommit}";
+
+      # src = pkgs.fetchFromGitHub {
+      #   owner = "niri-wm";
+      #   repo = "niri";
+      #   rev = niriCommit;
+      #   hash = "sha256-lqweVTwHhYc+9T33cysp38gVwxaibGJHriOPZXWyhCY=";
+      # };
+
+      # cargoDeps = oldAttrs.cargoDeps.overrideAttrs (pkgs.lib.const {
+      #   inherit src patches;
+      #   outputHashMode = "recursive";
+      #   outputHashAlgo = "sha256";
+      #   outputHash = "sha256-WEk60uhpXuQk5szOKohnjMYz7uq+jvnRfyMKUAz3BLM=";
+      # });
+
+      # postPatch = ''
+      #   patchShebangs resources/niri-session
+      # '';
+
+      # doCheck = false;
       patches = (oldAttrs.patches or [ ]) ++ [
-        ./patches/niri-shm.patch
+        ./patches/niri-shm-2511.patch
       ];
-      #env = (oldAttrs.env or { }) // {
-      #  RUSTFLAGS = "-C target-cpu=native";
-      #  CARGO_PROFILE_RELEASE_OPT_LEVEL = "3";
-      #  CARGO_PROFILE_RELEASE_LTO = "fat";
-      #};
     });
   };
 
