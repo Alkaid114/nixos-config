535388b372de05e65754d04e6a3443f425c41f02
Alkaid <a1kari8@outlook.com>
2026-09-02 14:17:25 +0800

    添加ai相关工具，移除bun和python3

--- home/coding/default.nix
+++ home/coding/default.nix
@@ -1,22 +1,20 @@
 {
   pkgs,
-  config,
   ...
 }:
 {
   imports = [
     ./git.nix
     ./vscode.nix
+    ./ai.nix
   ];
 
   home.packages = with pkgs; [
     uv
     filezilla
-    bun
-    python3
   ];
 
-  home.sessionVariables.PATH = "${pkgs.bun}/bin:${config.home.homeDirectory}/.bun/bin:$PATH";
+  # home.sessionVariables.PATH = "${pkgs.bun}/bin:${config.home.homeDirectory}/.bun/bin:$PATH";
 
   home.sessionPath = [
     "$HOME/.local/bin"
