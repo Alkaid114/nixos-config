1e215953ca5217104f6a353c59ca76f0d6f3b3df
Alkaid <a1kari8@outlook.com>
2026-09-02 18:41:29 +0800

    UEFI启动目标改名，修改boot挂载权限

--- modules/system/boot.nix
+++ modules/system/boot.nix
@@ -1,28 +1,27 @@
 {
-  inputs,
   pkgs,
   lib,
   ...
 }:
 {
+  fileSystems."/boot".options = lib.mkForce [ "umask=0077" ];
+
   boot = {
     loader = {
       efi = {
         canTouchEfiVariables = true;
         efiSysMountPoint = "/boot";
       };
-      # grub = {
-      #   enable = true;
-      #   device = "nodev";
-      #   efiSupport = true;
-      #   useOSProber = true;
-      #   theme = lib.mkForce inputs.nixos-grub-themes.packages.${pkgs.system}.nixos;
-      # };
       limine = {
         enable = false;
         efiSupport = true;
       };
-      systemd-boot.enable = true;
+      systemd-boot = {
+        enable = true;
+        extraInstallCommands = ''
+          ${pkgs.systemd}/bin/bootctl install --esp-path=/boot --efi-boot-option-description=NixOS
+        '';
+      };
     };
     kernelPackages = pkgs.linuxPackages_zen;
     kernel.sysctl = {
